caf421d5b87447cfe46db6bb06bd61dff9a62291
Niken Wulandari <nikenwulandari111@gmail.com>
2019-11-12 21:33:58 +0700

    tidy up

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,14 +1,17 @@
 <template>
-    <section id="schedule">
-        <div class="container">
-            We will update soon!
-        </div>
-    </section>
+  <section id="schedule">
+    <div class="container">
+      We will update soon!
+    </div>
+  </section>
 </template>
 
 
-<style lang="sass" scoped>
-    #schedule
-        background-color: #e8f0fe
-        padding: 100px 0
+<style
+  lang="sass"
+  scoped
+>
+  #schedule
+    background-color: #e8f0fe
+    padding: 100px 0
 </style>
