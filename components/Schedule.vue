521be88183a273ad7fd142aebe85209a74b8368a
Niken Wulandari <nikenwulandari111@gmail.com>
2019-11-12 22:25:22 +0700

    create table rundown

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,7 +1,64 @@
 <template>
   <section id="schedule">
     <div class="container">
-      We will update soon!
+      <div id="schedule-row">
+        <table class="table">
+          <tbody>
+            <tr>
+              <td>09:00-09:45</td>
+              <td>Registration</td>
+            </tr>
+            <tr>
+              <td>09:45-10:00</td>
+              <td>Opening by GDG Jogjakarta</td>
+            </tr>
+            <tr>
+              <td>10:00-10:15</td>
+              <td>Welcoming Notes from BSI UII</td>
+            </tr>
+            <tr>
+              <td>10:15-10:30</td>
+              <td>Humas Kominfo</td>
+            </tr>
+            <tr>
+              <td>10:30-11:15</td>
+              <td>Google Cloud Codelabs</td>
+            </tr>
+            <tr>
+              <td>11:15-12:00</td>
+              <td>Machine Learning on GCP Codelabs</td>
+            </tr>
+            <tr>
+              <td>12:00-13:00</td>
+              <td>Lunch break</td>
+            </tr>
+            <tr>
+              <td>13:00-13:45</td>
+              <td>Say Hi! to Jetpack Compose</td>
+            </tr>
+            <tr>
+              <td>13:45-14:30</td>
+              <td>AMP</td>
+            </tr>
+            <tr>
+              <td>14:30-14:45</td>
+              <td>Made for Indonesia (Google)</td>
+            </tr>
+            <tr>
+              <td>14:45-15:30</td>
+              <td>Flutter</td>
+            </tr>
+            <tr>
+              <td>15:30-16:15</td>
+              <td>Web Technologies</td>
+            </tr>
+            <tr>
+              <td>16:15-16:30</td>
+              <td>Closing & Networking</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </section>
 </template>
@@ -14,4 +71,8 @@
   #schedule
     background-color: #e8f0fe
     padding: 100px 0
+  #schedule-row
+    display: flex
+    flex-direction: row
+    justify-content: center
 </style>
