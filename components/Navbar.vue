0bd16542f4d722b199d1037db3f606f71f4d6d05
Erdiawan Anna <erdie.one@gmail.com>
2019-11-13 11:47:40 +0700

    add bootstrap package

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -51,6 +51,8 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+    @import "./node_modules/bootstrap/scss/bootstrap"
+    
     #navbar
         .navbar
             background-color: white
